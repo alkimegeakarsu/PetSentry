449abfe80d28f181da85e0e28383c903de1f755c
alkimegeakarsu <alkimegeakarsu@gmail.com>
2023-12-02 11:52:21 +0300

    Added Firebase dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -61,6 +62,10 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.5")
     implementation("com.amazonaws:ivs-player:1.22.0")
     implementation("com.google.android.exoplayer:exoplayer:2.18.7")
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-database")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
